0cdacd9c44b9a52b65cfe46529dc56f91a121620
double-em <meldgaardmike@gmail.com>
2021-05-13 13:17:02 +0200

    Initial

---
+++ RAPI/Data/RAPIContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RAPI.Models;
+
+namespace RAPI.Data
+{
+    public class RAPIContext : DbContext
+    {
+        public RAPIContext (DbContextOptions<RAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RAPI.Models.Product> Product { get; set; }
+    }
+}
---
+++ RAPI/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RAPI.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Unit { get; set; }
+        public int Amount { get; set; }
+        public decimal Price { get; set; }
+        public Category Category { get; set; }
+        public int StorageAmount { get; set; }
+        public Supplier Supplier { get; set; }
+    }
+}
